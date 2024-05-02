152f07a3be39a39ffc64f7d330246de3edb41064
Chido100 <chido_dozie@hotmail.com>
2024-05-02 22:07:04 +0100

    Updated the login.html file

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    path('login/', LoginView.as_view(template_name='account/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='account/logout.html'), name='logout'),
 ]
